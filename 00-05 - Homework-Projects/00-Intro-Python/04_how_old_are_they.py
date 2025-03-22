cdd87d6d1f37b94137e5e5ca0d609a7cbf5d9d54
Muhammad Shariq <muhammadshariqfazal@gmail.com>
2025-03-22 15:55:23 +0500

    04 - Assignment

---
+++ 00-05 - Homework-Projects/00-Intro-Python/04_how_old_are_they.py
@@ -0,0 +1,26 @@
+def main():
+    print("Welcome to How Old Are They Program!")
+    
+    Anton: int = 21
+    
+    Beth: int = Anton + 6
+    
+    Chen: int = Beth + 20
+    
+    Drew: int = Chen + Anton
+    
+    Ethan: int = Chen
+    
+    print(f"Anton is {Anton} years old.")
+    
+    print(f"Beth is {Beth} years old.")
+    
+    print(f"Chen is {Chen} years old.")
+    
+    print(f"Drew is {Drew} years old.")
+    
+    print(f"Ethan is {Ethan} years old.")
+    
+
+if __name__ == '__main__':
+    main()

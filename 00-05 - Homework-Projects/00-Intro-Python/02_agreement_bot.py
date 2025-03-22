d5c7418ef03914cb028facf86e6a485cd1f923de
Muhammad Shariq <muhammadshariqfazal@gmail.com>
2025-03-22 15:44:11 +0500

    02 - Assignment

---
+++ 00-05 - Homework-Projects/00-Intro-Python/02_agreement_bot.py
@@ -0,0 +1,11 @@
+def main():
+    print("Welcome to Agreement Bot Program")
+    
+    fvrt_animal: str = input("What is your favorite animal? ")
+    
+    print(f"My favorite animal is also \"{fvrt_animal}\"")
+
+
+
+if __name__ == '__main__':
+    main()
